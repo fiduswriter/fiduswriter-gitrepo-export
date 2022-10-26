8b8692ba2cd0c8d9138043b5722af5b5e2d0ae24
Johannes Wilm <mail@johanneswilm.org>
2022-10-26 19:59:16 +0200

    3.11.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="fiduswriter-gitrepo-export",
-    version="3.10.9",
+    version="3.11.0",
     packages=find_namespace_packages(),
     exclude_package_data={"": ["configuration.py", "django-admin.py", "build/*"]},
     include_package_data=True,
@@ -47,8 +47,6 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Django",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.1",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Affero General Public License v3",
