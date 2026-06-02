9de15ca671a46b963938cf5e5eb69f019f4b50d1
Johannes Wilm <mail@johanneswilm.org>
2026-06-02 11:52:28 +0200

    rm pandoc-wasm, fiduswriter-pandoc#108fae60bc06faaf6ce462f0106c707a78554ac4

--- fiduswriter/gitrepo_export/static/js/modules/gitrepo_export/pandoc_document_exporter.js
+++ fiduswriter/gitrepo_export/static/js/modules/gitrepo_export/pandoc_document_exporter.js
@@ -1,8 +1,6 @@
-import {convert} from "pandoc-wasm"
-import {get} from "../common"
-import {PandocExporter} from "../exporter/pandoc"
+import {PandocConversionExporter} from "../pandoc/exporter"
 
-export class PandocDocGitExporter extends PandocExporter {
+export class PandocDocGitExporter extends PandocConversionExporter {
     constructor(
         format,
         fileExtension,
@@ -15,65 +13,39 @@ export class PandocDocGitExporter extends PandocExporter {
         updated,
         repo
     ) {
-        super(doc, bibDB, imageDB, csl, updated)
-        this.format = format
-        this.fileExtension = fileExtension
-        this.mimeType = mimeType
-        this.options = options || {}
+        super(
+            format,
+            fileExtension,
+            mimeType,
+            options,
+            doc,
+            bibDB,
+            imageDB,
+            csl,
+            updated
+        )
         this.repo = repo
     }
 
     createExport() {
-        return Promise.all(
-            this.httpFiles.map(binaryFile =>
-                get(binaryFile.url)
-                    .then(response => response.blob())
-                    .then(blob =>
-                        Promise.resolve({
-                            contents: blob,
-                            filename: binaryFile.filename
-                        })
-                    )
-            )
-        )
-            .then(binaryFiles => {
-                const files = {}
-                this.textFiles.forEach(file => {
-                    files[file.filename] = file.contents
-                })
-                binaryFiles.forEach(file => {
-                    files[file.filename] = file.contents
-                })
-                const options = {
-                    from: "json",
-                    to: this.format,
-                    standalone: true
-                }
-                if (files["bibliography.bib"]) {
-                    options.bibliography = "bibliography.bib"
-                    options.citeproc = true
-                }
-                const content = JSON.stringify(this.conversion.json)
-                return convert(options, content, files)
+        return this.runPandocConversion().then(({stdout: out}) => {
+            if (this.options.fullFileExport) {
+                const blob =
+                    out instanceof Blob
+                        ? out
+                        : new Blob([out], {type: this.mimeType})
+                return this.returnSingleFile(blob)
+            }
+            this.textFiles.push({
+                filename: `document.${this.fileExtension}`,
+                contents: out
             })
-            .then(({stdout: out}) => {
-                if (this.options.fullFileExport) {
-                    const blob =
-                        out instanceof Blob
-                            ? out
-                            : new Blob([out], {type: this.mimeType})
-                    return this.returnSingleFile(blob)
-                }
-                this.textFiles.push({
-                    filename: `document.${this.fileExtension}`,
-                    contents: out
-                })
-                if (!this.options.includeBibliography) {
-                    this.textFiles = this.textFiles.filter(
-                        file => file.filename !== "bibliography.bib"
-                    )
-                }
-                return this.returnFiles()
-            })
+            if (!this.options.includeBibliography) {
+                this.textFiles = this.textFiles.filter(
+                    file => file.filename !== "bibliography.bib"
+                )
+            }
+            return this.returnFiles()
+        })
     }
 }
