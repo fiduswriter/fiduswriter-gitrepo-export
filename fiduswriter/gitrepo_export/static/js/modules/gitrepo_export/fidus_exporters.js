83a2867bf93939ec3b642dc944d93711b30d75e3
Johannes Wilm <mail@johanneswilm.org>
2026-07-14 11:32:34 +0200

    Declare npm dependencies for extracted @fiduswriter packages

--- fiduswriter/gitrepo_export/static/js/modules/gitrepo_export/fidus_exporters.js
+++ fiduswriter/gitrepo_export/static/js/modules/gitrepo_export/fidus_exporters.js
@@ -2,7 +2,7 @@ import {FIDUSBOOK_VERSION} from "@fiduswriter/books-document/exporter/native"
 import {getMissingChapterData} from "@fiduswriter/books-document/exporter/tools"
 import {ShrinkFidus} from "@fiduswriter/document/exporter/native/shrink"
 import {ZipFidus} from "@fiduswriter/document/exporter/native/zip"
-import {ZipFileCreator} from "@fiduswriter/document/exporter/tools/zip"
+import {ZipFileCreator} from "fwtoolkit/file/zip"
 import {addAlert, gettext} from "fwtoolkit"
 
 /**
